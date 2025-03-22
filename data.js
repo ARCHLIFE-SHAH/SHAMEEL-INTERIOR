2e026557b33ee8aeb775a718e9ccc7147854b64d
ARCHLIFE-SHAH <archlife0@gmail.com>
2025-03-22 20:25:54 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,363 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-1",
+      "name": "LIVING 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.031013306183034217,
+        "pitch": 0.004281462032192707,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9153280425020451,
+          "pitch": 0.2760453379829464,
+          "rotation": 0,
+          "target": "2-l3"
+        },
+        {
+          "yaw": 2.3088630203137326,
+          "pitch": 0.42370413388610473,
+          "rotation": 3.141592653589793,
+          "target": "1-l2"
+        },
+        {
+          "yaw": 1.5402171406699408,
+          "pitch": 0.24850405771634954,
+          "rotation": 6.283185307179586,
+          "target": "3-dinning--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-l2",
+      "name": "L2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.23681650255792164,
+        "pitch": -0.00433568338092627,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.046076973092775475,
+          "pitch": 0.2034603379242821,
+          "rotation": 0,
+          "target": "2-l3"
+        },
+        {
+          "yaw": 1.5604455005336284,
+          "pitch": 0.1417508802662013,
+          "rotation": 5.497787143782138,
+          "target": "4-dinning--2"
+        },
+        {
+          "yaw": -0.17020939152934744,
+          "pitch": 0.6765169512022275,
+          "rotation": 7.0685834705770345,
+          "target": "0-living-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-l3",
+      "name": "L3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05297240452028973,
+          "pitch": 0.23111772015735,
+          "rotation": 0,
+          "target": "1-l2"
+        },
+        {
+          "yaw": -0.505745356825205,
+          "pitch": 0.22054439848010077,
+          "rotation": 5.497787143782138,
+          "target": "3-dinning--1"
+        },
+        {
+          "yaw": 0.22130024405304916,
+          "pitch": 0.5530675693206675,
+          "rotation": 10.210176124166829,
+          "target": "0-living-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dinning--1",
+      "name": "DINNING -1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.03918086075905691,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2978343020161276,
+          "pitch": 0.21587255634114655,
+          "rotation": 1.5707963267948966,
+          "target": "0-living-1"
+        },
+        {
+          "yaw": 1.278828068492098,
+          "pitch": 0.08144676252766203,
+          "rotation": 5.497787143782138,
+          "target": "6-wash-area"
+        },
+        {
+          "yaw": 2.0638225869753715,
+          "pitch": 0.2059592459702202,
+          "rotation": 0,
+          "target": "4-dinning--2"
+        },
+        {
+          "yaw": 0.5681875254590665,
+          "pitch": 0.10668855190973403,
+          "rotation": 4.71238898038469,
+          "target": "5-courtyard-area"
+        },
+        {
+          "yaw": -0.687877071794599,
+          "pitch": 0.21612518654596968,
+          "rotation": 5.497787143782138,
+          "target": "1-l2"
+        },
+        {
+          "yaw": -0.04890441099942855,
+          "pitch": 0.07025940506172113,
+          "rotation": 0.7853981633974483,
+          "target": "2-l3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dinning--2",
+      "name": "DINNING -2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.13521089063078762,
+        "pitch": 0.01662606950276313,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0455384476483118,
+          "pitch": 0.13775917414873362,
+          "rotation": 7.853981633974483,
+          "target": "1-l2"
+        },
+        {
+          "yaw": -1.4907990299069294,
+          "pitch": 0.31403391376823286,
+          "rotation": 0,
+          "target": "3-dinning--1"
+        },
+        {
+          "yaw": 0.7702858741743146,
+          "pitch": 0.33519352930380464,
+          "rotation": 0,
+          "target": "6-wash-area"
+        },
+        {
+          "yaw": -0.3040069870498492,
+          "pitch": 0.03491020014513424,
+          "rotation": 1.5707963267948966,
+          "target": "5-courtyard-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-courtyard-area",
+      "name": "COURTYARD AREA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.18967002483118733,
+        "pitch": 0.4291012330122559,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0560571206398599,
+          "pitch": 0.1980304123549832,
+          "rotation": 5.497787143782138,
+          "target": "6-wash-area"
+        },
+        {
+          "yaw": 0.5137101232661845,
+          "pitch": 0.14464058252229606,
+          "rotation": 0,
+          "target": "4-dinning--2"
+        },
+        {
+          "yaw": 0.9083262175598144,
+          "pitch": 0.16919860912721774,
+          "rotation": 1.5707963267948966,
+          "target": "3-dinning--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-wash-area",
+      "name": "WASH AREA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.0676780195120017,
+        "pitch": 0.06615078620425052,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.42308934885832805,
+          "pitch": 0.20587845980918829,
+          "rotation": 0,
+          "target": "5-courtyard-area"
+        },
+        {
+          "yaw": -1.707244809973023,
+          "pitch": 0.18253329719548006,
+          "rotation": 0,
+          "target": "3-dinning--1"
+        },
+        {
+          "yaw": -1.3881089639828286,
+          "pitch": 0.06591006725832393,
+          "rotation": 1.5707963267948966,
+          "target": "1-l2"
+        },
+        {
+          "yaw": -2.2773188031885105,
+          "pitch": 0.34456650154130486,
+          "rotation": 5.497787143782138,
+          "target": "4-dinning--2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SHAMEEL- INTERIOR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
